333bd040dc417cb2c600df0bd49e14cd5488b5c4
LuanRoger <luan.roger.2003@gmail.com>
2023-12-17 20:15:48 -0300

    feat: create `WavePossitionArea`

--- src/WFC/WavePossition.cs
+++ src/WFC/WavePossition.cs
@@ -1,10 +1,10 @@
 ﻿namespace RoguelikeWFC.WFC;
 
-public struct WavePossition(byte[] entropy)
+public readonly struct WavePossition(byte[] entropy)
 {
     public readonly byte[] Entropy = entropy;
     public bool collapsed => Entropy.Length == 1;
-    public bool hasConflict => Entropy.Length == 0;
+    public bool conflict => Entropy.Length == 0;
 
     public static explicit operator WavePossition(byte[] entropy) => 
         new(entropy);
