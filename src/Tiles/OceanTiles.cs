4abca945deff305d237bd737ae903b2de6420bbb
LuanRoger <luan.roger.2003@gmail.com>
2023-12-20 14:29:48 -0300

    feat: fit all `MapTile` sockets at once

--- src/Tiles/OceanTiles.cs
+++ src/Tiles/OceanTiles.cs
@@ -2,45 +2,25 @@
 
 namespace RoguelikeWFC.Tiles;
 
-public class OceanCoastGrass() : MapTile(TileIDs.OceanCoastGrass, 249, Color.AnsiGreen, new()
-{
-    fitTop = new byte[] { TileIDs.OceanCoastGrass, TileIDs.OceanCoatsSand },
-    fitRight = new byte[] { TileIDs.OceanCoastGrass, TileIDs.OceanCoatsSand },
-    fitBottom = new byte[] { TileIDs.OceanCoastGrass, TileIDs.OceanCoatsSand },
-    fitLeft = new byte[] { TileIDs.OceanCoastGrass, TileIDs.OceanCoatsSand }
-},  background: Color.GreenYellow);
+public class OceanCoastGrass() : MapTile(TileIDs.OceanCoastGrass, 249, Color.AnsiGreen, 
+    new(new byte[] { TileIDs.OceanCoastGrass, TileIDs.OceanCoatsSand }),  
+    background: Color.GreenYellow);
 
-public class OceanCoastSand() : MapTile(TileIDs.OceanCoatsSand, 178, Color.Yellow, new()
-{
-    fitTop = new byte[] { TileIDs.OceanCoatsSand, TileIDs.OceanCoastGrass, TileIDs.Water },
-    fitRight = new byte[] { TileIDs.OceanCoatsSand, TileIDs.OceanCoastGrass, TileIDs.Water },
-    fitBottom = new byte[] { TileIDs.OceanCoatsSand, TileIDs.OceanCoastGrass, TileIDs.Water },
-    fitLeft = new byte[] { TileIDs.OceanCoatsSand, TileIDs.OceanCoastGrass, TileIDs.Water }
-},  background: Color.GreenYellow);
+public class OceanCoastSand() : MapTile(TileIDs.OceanCoatsSand, 178, Color.Yellow, 
+    new(new byte[] { TileIDs.OceanCoatsSand, TileIDs.OceanCoastGrass, TileIDs.Water }),
+    background: Color.GreenYellow);
 
-public class WatterTile() : MapTile(TileIDs.Water, 247, Color.AnsiCyan, new()
-{
-    fitTop = new byte[] { TileIDs.Water, TileIDs.Ocean, TileIDs.OceanCoatsSand },
-    fitRight = new byte[] { TileIDs.Water, TileIDs.Ocean, TileIDs.OceanCoatsSand },
-    fitBottom = new byte[] { TileIDs.Water, TileIDs.Ocean, TileIDs.OceanCoatsSand },
-    fitLeft = new byte[] { TileIDs.Water, TileIDs.Ocean, TileIDs.OceanCoatsSand }
-},  background: Color.Aqua);
+public class WatterTile() : MapTile(TileIDs.Water, 247, Color.AnsiCyan, 
+    new(new byte[] { TileIDs.Water, TileIDs.Ocean, TileIDs.OceanCoatsSand }), 
+    background: Color.Aqua);
 
-public class OceanTile() : MapTile(TileIDs.Ocean, 247, Color.AnsiBlue, new()
-{
-    fitTop = new byte[] { TileIDs.Ocean, TileIDs.DeepOcean, TileIDs.Water },
-    fitRight = new byte[] { TileIDs.Ocean, TileIDs.DeepOcean, TileIDs.Water },
-    fitBottom = new byte[] { TileIDs.Ocean, TileIDs.DeepOcean, TileIDs.Water },
-    fitLeft = new byte[] { TileIDs.Ocean, TileIDs.DeepOcean, TileIDs.Water }
-},  background: Color.Aqua);
+public class OceanTile() : MapTile(TileIDs.Ocean, 247, Color.AnsiBlue, 
+    new(new byte[] { TileIDs.Ocean, TileIDs.DeepOcean, TileIDs.Water }), 
+    background: Color.Aqua);
 
-public class DeepOceanTile() : MapTile(TileIDs.DeepOcean, 247, Color.DarkBlue, new()
-{
-    fitTop = new byte[] { TileIDs.DeepOcean, TileIDs.Ocean },
-    fitRight = new byte[] { TileIDs.DeepOcean, TileIDs.Ocean },
-    fitBottom = new byte[] { TileIDs.DeepOcean, TileIDs.Ocean },
-    fitLeft = new byte[] { TileIDs.DeepOcean, TileIDs.Ocean }
-},  background: Color.Aqua);
+public class DeepOceanTile() : MapTile(TileIDs.DeepOcean, 247, Color.DarkBlue, 
+    new(new byte[] { TileIDs.DeepOcean, TileIDs.Ocean }), 
+    background: Color.Aqua);
 
 public class OceanAtlas() : TileAtlas(new MapTile[]
 {
--- src/WFC/TileSocket.cs
+++ src/WFC/TileSocket.cs
@@ -2,16 +2,32 @@
 
 public struct TileSocket
 {
-    public static TileSocket Empty => new()
+    public static TileSocket Empty => new();
+
+    public TileSocket()
     {
-        fitTop = Array.Empty<byte>(),
-        fitRight = Array.Empty<byte>(),
-        fitBottom = Array.Empty<byte>(),
-        fitLeft = Array.Empty<byte>()
-    };
+        fitTop = Array.Empty<byte>();
+        fitRight = Array.Empty<byte>();
+        fitBottom = Array.Empty<byte>();
+        fitLeft = Array.Empty<byte>();
+    }
+
+    public TileSocket(byte[] fit)
+    {
+        int fitRuleLength = fit.Length;
+        fitTop = new byte[fitRuleLength];
+        fitRight = new byte[fitRuleLength];
+        fitBottom = new byte[fitRuleLength];
+        fitLeft = new byte[fitRuleLength];
+        
+        Array.Copy(fit, fitTop, fitRuleLength);
+        Array.Copy(fit, fitRight, fitRuleLength);
+        Array.Copy(fit, fitBottom, fitRuleLength);
+        Array.Copy(fit, fitLeft, fitRuleLength);
+    }
     
-    public required byte[] fitTop { get; init; }
-    public required byte[] fitRight { get; init; }
-    public required byte[] fitBottom { get; init; }
-    public required byte[] fitLeft { get; init; }
+    public byte[] fitTop { get; init; }
+    public byte[] fitRight { get; init; }
+    public byte[] fitBottom { get; init; }
+    public byte[] fitLeft { get; init; }
 }
